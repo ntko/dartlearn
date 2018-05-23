46598f4dcdf82adcbacd0ced49321930bdfb4c03
Tanger John <ntko@users.noreply.github.com>
2018-05-23 18:04:40 +0800

    init commit

---
+++ consoleapp/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:consoleapp/consoleapp.dart' as consoleapp;
+
+main(List<String> arguments) {
+  print('Hello world: ${consoleapp.calculate()}!');
+}
---
+++ consoleapp/test/consoleapp_test.dart
@@ -0,0 +1,8 @@
+import 'package:consoleapp/consoleapp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
