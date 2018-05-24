adbc69c8e2d19a275dbbb5f5a2b4a3684010662b
Tanger John <ntko@users.noreply.github.com>
2018-05-25 01:54:46 +0800

    import use single quoter

--- consoleapp/bin/main.dart
+++ consoleapp/bin/main.dart
@@ -1,4 +1,4 @@
-import "package:consoleapp/consoleapp.dart" as consoleapp;
+import 'package:consoleapp/consoleapp.dart' as consoleapp;
 
 main(List<String> arguments) {
   print('Hello world: ${consoleapp.calculate()}!');
